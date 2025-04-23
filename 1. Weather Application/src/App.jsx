4cd6c2886a385e617f29de749fa08f2313db89fb
Farhana Bente Islam <21303195@iubat.edu>
2025-04-23 22:41:46 +0600

    2nd commit

---
+++ 1. Weather Application/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Router from "./router/routers"
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Router/>     
+    </>
+  )
+}
+
+export default App
